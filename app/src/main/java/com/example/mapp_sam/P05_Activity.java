76eb130fdf212466abdfea5fd5d83ab3d1e55928
sammy3620 <50613536+sammy3620@users.noreply.github.com>
2019-10-24 09:53:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mapp_sam/P05_Activity.java
@@ -0,0 +1,14 @@
+package com.example.mapp_sam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class P05_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p05_);
+    }
+}
