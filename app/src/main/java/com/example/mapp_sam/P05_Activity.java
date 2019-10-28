155166e2a8b47945183523bc72e403398d8642a1
sammy3620 <50613536+sammy3620@users.noreply.github.com>
2019-10-28 20:47:36 +0800

    new prac

--- app/src/main/java/com/example/mapp_sam/P05_Activity.java
+++ app/src/main/java/com/example/mapp_sam/P05_Activity.java
@@ -3,12 +3,20 @@ package com.example.mapp_sam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class P05_Activity extends AppCompatActivity {
-
+    String msg = "P05_Activity Sam";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_p05_);
+
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
 }
