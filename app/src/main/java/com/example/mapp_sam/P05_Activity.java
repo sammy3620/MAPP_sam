4d59d33ee298d0f97c1fad5aea92065e5e89f352
sammy3620 <50613536+sammy3620@users.noreply.github.com>
2019-11-11 16:45:52 +0800

    prac

--- app/src/main/java/com/example/mapp_sam/P05_Activity.java
+++ app/src/main/java/com/example/mapp_sam/P05_Activity.java
@@ -19,6 +19,27 @@ public class P05_Activity extends AppCompatActivity {
         super.onStart();
         Log.d(msg, "The onStart() event");
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+
 
     public	void	launchSecondActivity(View view)	{
         Log.d("MainActivity",	"Button clicked!");
